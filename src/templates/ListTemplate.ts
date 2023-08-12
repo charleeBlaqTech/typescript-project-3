528300d205bb1789abb924a93cd4238b1c5ad34b
Daudu Arunegbode charles <blaqwayschannel@gmail.com>
2023-08-12 22:56:07 +0100

    listTemplate created and all class logics included

--- src/templates/ListTemplate.ts
+++ src/templates/ListTemplate.ts
@@ -5,3 +5,54 @@ interface DOMList{
     clear(): void,
     render(fulllList: FullList): void
 }
+
+
+export default class ListTemplate implements DOMList{
+    ul: HTMLUListElement
+
+    private constructor(){
+        this.ul = document.getElementById('listItems') as HTMLUListElement
+    }
+
+    clear(): void {
+        this.ul.innerHTML= ""
+    }
+
+    render(fulllList: FullList): void {
+        this.clear();
+
+        fulllList.list.forEach(item=>{
+                const li = document.createElement('li') as HTMLElement;
+                li.className= "item";
+
+                const check = document.createElement('input') as HTMLInputElement;
+                check.type = "checkbox";
+                check.id = item.id;
+                check.checked = item.checked;
+                
+
+                check.addEventListener('change', ()=>{
+                    item.checked = !item.checked;
+                    fulllList.save();
+                })
+
+                const label = document.createElement('label') as HTMLLabelElement;
+                label.htmlFor = item.id;
+                label.textContent = item.item;
+
+               
+
+                const button = document.createElement('button') as HTMLButtonElement;
+                button.className = 'button';
+                button.textContent= "X";
+                button.addEventListener('click', ()=>{
+                    fulllList.removeItem(item.id);
+                    this.render(fulllList);
+                })
+
+                li.append(check, label, button)
+
+
+        })
+    }
+}
